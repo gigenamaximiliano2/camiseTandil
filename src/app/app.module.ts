17d4b32002c28b2e466c4798838078c5df656a6a
gigenamaximiliano2 <gigenamaximiliano2@gmail.com>
2024-07-03 15:46:15 -0300

    corrigen errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,17 @@ import { AppComponent } from './app.component';
 import { CamListComponent } from './cam-list/cam-list.component';
 
 import { FormsModule } from '@angular/forms';
-import { CamAboutComponent } from './cam-about/cam-about.component';
-import { CamCamTandilComponent } from './cam-cam-tandil/cam-cam-tandil.component';
 import { CarritoComponent } from './carrito/carrito.component';
 import { InputIntegerComponent } from './input-integer/input-integer.component';
+import { CamCamTandilComponent } from './cam-cam-tandil/cam-cam-tandil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CamListComponent,
-    CamAboutComponent,
-    CamCamTandilComponent,
     CarritoComponent,
-    InputIntegerComponent
+    InputIntegerComponent,
+    CamCamTandilComponent
   ],
   imports: [
     BrowserModule,
